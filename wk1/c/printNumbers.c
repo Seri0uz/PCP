d8887225d2ebcc50d7f24a30ee9aebf8303b610a
samuelduss <saemiduss@gmail.com>
2026-09-24 09:55:53 +0200

    added exercise one and two

---
+++ wk1/c/printNumbers.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void printNumbersGoto(int n) {
+    int i = 0;
+    start:
+        if (i <= n)
+            goto counter;
+        else
+            return;
+
+    counter:
+        printf ("%d ", i);
+        i++;
+        goto start;
+    }
+
+void printNumbersFor(int n) {
+    for (int i = 0; i <= n; i++) {
+        printf ("%d ", i);
+        }
+    }
+
+
+void printNumbersRecursiveFunction (int n) {
+    if (n > 0)
+        printNumbersRecursiveFunction (n - 1);
+    printf ("%d ", n);
+    }
+
+void printReverseNumbersRecursiveFunction(int n) {
+    printf ("%d ", n);
+    if (n > 0)
+        printReverseNumbersRecursiveFunction (n - 1);
+    }
+
+int main(){
+    printNumbersGoto(7);
+    printf("= printNumbersGoto(7)\n");
+    printNumbersFor(7);
+    printf("= printNumbersFor(7)\n");
+    printNumbersRecursiveFunction(7);
+    printf("= printNumbersRecursiveFunction(7)\n");
+    printReverseNumbersRecursiveFunction(7);
+    printf("= printReverseNumbersRecursiveFunction(7)\n");
+    }
